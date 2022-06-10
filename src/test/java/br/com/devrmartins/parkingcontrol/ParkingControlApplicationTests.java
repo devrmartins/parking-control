1dcc5914accc5cafc6ab47945fa9702518b591f5
Raffael Martins <rbarbosama@indracompany.com>
2022-06-10 17:18:15 -0300

    Commit inicial

---
+++ src/test/java/br/com/devrmartins/parkingcontrol/ParkingControlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devrmartins.parkingcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
